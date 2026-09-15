6888e132150df08f2cd545aecd8bf508447db95b
nhoeunmengkoung <nhoeunmengkoung42@gmail.com>
2026-09-15 23:01:13 +0700

    initial: create student registration system

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "========================================" << endl;
+    cout << "   STUDENT COURSE REGISTRATION SYSTEM" << endl;
+    cout << "========================================" << endl;
+
+    cout << "System started successfully!" << endl;
+
+    return 0;
+}
